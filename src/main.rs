3aceaef0d41b3945a12bd2690477831bd6a5c851
Priyansh Shah <Priyanshpokemon@gmail.com>
2024-01-23 06:10:53 -0800

    Added the handle connection function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+        println!("Connection Established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer =  [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    // Reading modifies internal stream so we send a mutable reference
+    // Unwrap so if error panic!
+    // println!(
+    //     "Request {}",
+    //     String::from_utf8_lossy(&buffer[..])
+    //     // slice of bytes become a string including invalid characters
+    // )
+    
 }
